77b5e85d1c7834318776092ca55b28d963401a36
Suhira30 <suhirabalarajan@gmail.com>
2024-07-23 22:32:17 +0530

    -

--- src/Pages/IndividualBlog.js
+++ src/Pages/IndividualBlog.js
@@ -1,7 +1,36 @@
 import React from 'react'
 
 export const IndividualBlog = () => {
+  const backgroundStyle = {
+    backgroundImage: `url(${background})`,
+    height: '100vh',
+    backgroundSize: 'cover',
+    backgroundPosition: 'center',
+    backgroundRepeat: 'no-repeat',
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    overflow: 'hidden',
+    margin: 0,
+    padding: 0,
+  };
+
+  const [page, setPage] = React.useState(1);
+  const itemsPerPage = 12;
+  const pageCount = Math.ceil(itemData.length / itemsPerPage);
+
+  const handleChange = (event, value) => {
+    setPage(value);
+  };
+
+  const paginatedItems = itemData.slice((page - 1) * itemsPerPage, page * itemsPerPage);
+
   return (
-    <div>IndividualBlog</div>
-  )
+    <>
+      <div style={backgroundStyle}>
+        <Header />
+      </div>
+      
+    </>
+  );
 }
